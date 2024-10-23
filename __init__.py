e538b6f3377924bf7b70bd1933982a327cb54e40
rupayan <rupayan@insynkstudios.com>
2024-10-23 19:48:00 +0530

    Refactor code to update node class mappings and display names

--- __init__.py
+++ __init__.py
@@ -6,18 +6,18 @@ from .api.log import *
 
 from .nodes.s3_utils import UploadModelToS3
 from .nodes.model_utils import LoadModelFromCivitAI
-from .nodes.flowscale_volume_utils import UploadModelToFlowscaleVolume
+from .nodes.flowscale_volume_utils import SaveModelToFlowscaleVolume
 
 load_dotenv()
 
 NODE_CLASS_MAPPINGS = { 
   "UploadModelToS3": UploadModelToS3,
   "LoadModelFromCivitAI": LoadModelFromCivitAI,
-  "UploadModelToFlowscaleVolume": UploadModelToFlowscaleVolume
+  "SaveModelToFlowscaleVolume": SaveModelToFlowscaleVolume
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
   "UploadModelToS3": "[FS] Upload Model to S3",
   "LoadModelFromCivitAI": "[FS] Load Model from CivitAI",
-  "UploadModelToFlowscaleVolume": "[FS] Upload Model to Flowscale Volume"
+  "SaveModelToFlowscaleVolume": "[FS] Save Model to Flowscale Volume"
 }
--- nodes/flowscale_volume_utils.py
+++ nodes/flowscale_volume_utils.py
@@ -8,32 +8,44 @@ ACCESS_TOKEN = os.environ.get("FLOWSCALE_ACCESS_TOKEN")
 TEAM_ID = os.environ.get("FLOWSCALE_TEAM_ID")
 API_URL = os.environ.get("FLOWSCALE_API_URL")
 
-class UploadModelToFlowscaleVolume:
+class SaveModelToFlowscaleVolume:
   """ 
-  Uploads a model to a Flowscale volume.
+  Save a model to a Flowscale volume.
   """
   
   @classmethod
   def INPUT_TYPES(cls):
     return {
       "required": {
+          "model_type": (["lora", "controlnet", "vae", "unet", "other"],),
           "path_in_volume": ("STRING", {"multiline": False, "placeholder": "path/to/model, e.g. loras/my_model"})
       },
       "optional": {
           "huggingface_url": ("STRING", {"multiline": False, "forceInput": True}),
           "s3_url": ("STRING", {"multiline": False, "forceInput": True}),
-          "civilai_url": ("STRING", {"multiline": False, "forceInput": True})
+          "civitai_url": ("STRING", {"multiline": False, "forceInput": True})
       }
     }
     
   RETURN_TYPES = ("STRING",)
-  RETURN_NAMES = ("status",)
+  RETURN_NAMES = ("download_url",)
   FUNCTION = "upload_model_to_flowscale_volume"
   CATEGORY = "Utilities"
+  OUTPUT_NODE = True
   
-  def upload_model_to_flowscale_volume(self, s3_url, path_in_volume):
+  def upload_model_to_flowscale_volume(self, model_type, path_in_volume, huggingface_url=None, s3_url=None, civitai_url=None):
     if not all([VOLUME_ID, CONTAINER_ID, API_URL]):
       raise Exception("Flowscale credentials not set")
+    
+    if not any([huggingface_url, s3_url, civitai_url]):
+      raise Exception("No model URL provided")
+    
+    if huggingface_url:
+      download_url = huggingface_url
+    elif s3_url:
+      download_url = s3_url
+    elif civitai_url:
+      download_url = civitai_url
     
     url = f"{API_URL}/api/v1/volume/{VOLUME_ID}/upload?access_token={ACCESS_TOKEN}"
     headers = {
@@ -41,21 +53,15 @@ class UploadModelToFlowscaleVolume:
     }
     body = {
       "path": path_in_volume,
-      "download_url": s3_url,
+      "download_url": download_url,
       "upload_type": "lora",
     }
-    
-    curl_command = f"curl -X POST {url} -H 'X-Team: {TEAM_ID}' -H 'Content-Type: application/json' -d '{json.dumps(body)}'"
-    print(curl_command)
-    
+        
     response = httpx.post(url, headers=headers, json=body)
     if response.status_code != 200:
       raise Exception(f"Failed to upload model to Flowscale volume: {response.text}")
     
-    return {"ui": {"text": "Model Upload Initialized"}, "result": ("Model Upload Initialized",)}  
-    
-    
-    
+    return {"ui": {"text": download_url}, "result": (download_url,)}
     
     
     
