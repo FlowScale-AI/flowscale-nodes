a19d212171781ffecbb2f5ea49ed62388e80f673
rupayan <rupayan@insynkstudios.com>
2024-10-23 12:12:57 +0530

    Refactor code to update imports in __init__.py and add python-dotenv to requirements.txt

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 print("Initializing Flowscale Nodes")
+from dotenv import load_dotenv
 
 from .api.io import * 
 from .api.log import *
@@ -6,6 +7,8 @@ from .api.log import *
 from .nodes.s3_utils import UploadModelToS3
 from .nodes.model_utils import LoadModelFromCivitAI
 from .nodes.flowscale_volume_utils import UploadModelToFlowscaleVolume
+
+load_dotenv()
 
 NODE_CLASS_MAPPINGS = { 
   "Upload Model To S3": UploadModelToS3,
