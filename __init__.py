74392b150760cfd1cfea95b73bb8fd90b2e072ce
rupayan <rupayan@insynkstudios.com>
2024-11-20 18:46:17 +0530

    feat: add UploadModelToS3 class for uploading models to S3 with enhanced filepath handling

--- __init__.py
+++ __init__.py
@@ -4,13 +4,14 @@ from dotenv import load_dotenv
 from .api.io import * 
 from .api.log import *
 
-from .nodes.s3_utils import UploadModelToPublicS3, UploadModelToPrivateS3, LoadModelFromPublicS3, LoadModelFromPrivateS3
+from .nodes.s3_utils import UploadModelToS3, UploadModelToPublicS3, UploadModelToPrivateS3, LoadModelFromPublicS3, LoadModelFromPrivateS3
 from .nodes.model_utils import LoadModelFromCivitAI
 from .nodes.flowscale_volume_utils import SaveModelToFlowscaleVolume
 
 load_dotenv()
 
 NODE_CLASS_MAPPINGS = { 
+  "UploadModelToS3": UploadModelToS3,
   "UploadModelToPublicS3": UploadModelToPublicS3,
   "UploadModelToPrivateS3": UploadModelToPrivateS3,
   "LoadModelFromPublicS3": LoadModelFromPublicS3,
@@ -20,6 +21,7 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
+  "UploadModelToS3": "[FS] Upload Model to S3",
   "UploadModelToPublicS3": "[FS] Upload Model to Public S3",
   "UploadModelToPrivateS3": "[FS] Upload Model to Private S3",
   "LoadModelFromPublicS3": "[FS] Load Model from Public S3",
--- nodes/s3_utils.py
+++ nodes/s3_utils.py
@@ -12,6 +12,66 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY")
 AWS_REGION = os.environ.get("AWS_S3_REGION", "us-east-1")
 S3_BUCKET_NAME = os.environ.get("AWS_S3_BUCKET_NAME")
 
+
+class UploadModelToS3:
+  """
+  Uploads a model to S3
+  """
+  
+  @classmethod
+  def INPUT_TYPES(cls):
+    return {
+      "required": {
+          "filepath": ("STRING", {"forceInput": True}),
+          "model_name": ("STRING", {"forceInput": False})
+      },
+    }
+    
+  RETURN_TYPES = ("STRING", "STRING")
+  RETURN_NAMES = ("download_url", "model_name")
+  FUNCTION = "upload_model_to_s3"
+  CATEGORY = "Utilities"
+  
+  def upload_model_to_s3(self, filepath, model_name=None):
+    CONTAINER_ID = os.environ.get("CONTAINER_ID")
+    if filepath.startswith("./") or filepath.startswith("../"):
+        filepath = filepath.lstrip("./").lstrip("../")
+    if filepath.startswith("/") or filepath.startswith("\\"):
+        filepath = filepath.lstrip("/").lstrip("\\")
+
+    base_directory = os.getcwd()
+    sanitized_filepath = os.path.normpath(filepath).lstrip(os.sep).rstrip(os.sep)
+    absolute_filepath = os.path.abspath(os.path.join(base_directory, sanitized_filepath))
+    logger.info(f"Uploading model from {absolute_filepath}")
+    
+    if not all([AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, S3_BUCKET_NAME]):
+      raise Exception("AWS credentials not set")
+    
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_REGION
+    )
+    
+    if model_name:
+      if "." not in model_name and "." not in os.path.basename(absolute_filepath):
+        absolute_filepath += ".safetensors"
+      if "." not in model_name:
+        modified_model_name = model_name + ".safetensors"
+      s3_key = os.path.join("models", CONTAINER_ID, modified_model_name)
+    else:
+      if "." not in os.path.basename(absolute_filepath):
+        absolute_filepath += ".safetensors"
+      s3_key = os.path.join("models", CONTAINER_ID, os.path.basename(absolute_filepath))
+    
+    try:
+      s3_client.upload_file(absolute_filepath, S3_BUCKET_NAME, s3_key)
+      download_url = f"https://{S3_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{s3_key}"
+      return (download_url, model_name,)
+    except Exception as e:
+      raise Exception(f"Failed to upload model to S3: {str(e)}")
+
 class UploadModelToPublicS3:
   """
   Uploads a model to Public S3 Buckets
