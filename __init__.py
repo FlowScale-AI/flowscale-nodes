e6afc0274db710e2b632f97cfbf36b411cf46158
rupayan <rupayan@insynkstudios.com>
2025-01-28 15:04:22 +0530

    feat: add UploadTextToS3 to NODE_CLASS_MAPPINGS and NODE_DISPLAY_NAME_MAPPINGS

--- __init__.py
+++ __init__.py
@@ -5,10 +5,9 @@ from .api.io import *
 from .api.log import *
 from .api.custom_node import *
 
-from .nodes.s3_utils import UploadModelToS3, UploadModelToPublicS3, UploadModelToPrivateS3, LoadModelFromPublicS3, LoadModelFromPrivateS3, UploadImageToS3, UploadMediaToS3FromLink
+from .nodes.s3_utils import UploadModelToS3, UploadModelToPublicS3, UploadModelToPrivateS3, LoadModelFromPublicS3, LoadModelFromPrivateS3, UploadImageToS3, UploadMediaToS3FromLink, UploadTextToS3
 from .nodes.model_utils import LoadModelFromCivitAI
 from .nodes.flowscale_volume_utils import SaveModelToFlowscaleVolume
-from .nodes.time_utils import Delay
 from .nodes.webhook import WebhookSender
 
 load_dotenv()
@@ -17,6 +16,7 @@ NODE_CLASS_MAPPINGS = {
   "UploadModelToS3": UploadModelToS3,
   "UploadImageToS3": UploadImageToS3,
   "UploadMediaToS3FromLink": UploadMediaToS3FromLink,
+  "UploadTextToS3": UploadTextToS3,
   "UploadModelToPublicS3": UploadModelToPublicS3,
   "UploadModelToPrivateS3": UploadModelToPrivateS3,
   "LoadModelFromPublicS3": LoadModelFromPublicS3,
@@ -30,6 +30,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
   "UploadModelToS3": "[FS] Upload Model to S3",
   "UploadImageToS3": "[FS] Upload Image to S3",
   "UploadMediaToS3FromLink": "[FS] Upload Media to S3 from Link",
+  "UploadTextToS3": "[FS] Upload Text to S3",
   "UploadModelToPublicS3": "[FS] Upload Model to Public S3",
   "UploadModelToPrivateS3": "[FS] Upload Model to Private S3",
   "LoadModelFromPublicS3": "[FS] Load Model from Public S3",
