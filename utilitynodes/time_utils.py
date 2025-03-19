38b24bbc82f29a623bb6b7cbf84d4b6b81008e72
rupayan <rupayan@insynkstudios.com>
2025-03-19 19:23:26 +0530

    feat: enhance Delay class to include input validation and trigger handling

--- utilitynodes/time_utils.py
+++ utilitynodes/time_utils.py
@@ -5,30 +5,32 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class Delay:
-  """
-  Simulate network delay
-  """
-  
-  @classmethod
-  def INPUT_TYPES(cls):
-    return {
-      "required": {
-        "delay": ("INT",)
-      },
-      "optional": {
-        "trigger": ("*",)
-      }
-    }
+    """
+    Simulate network delay while passing through the input data
+    """
     
-  @classmethod
-  def VALIDATE_INPUTS(s, input_types):
-    return True
-  
-  RETURN_TYPES = ("*", )
-  RETURN_NAMES = ("delayed_response", )
-  FUNCTION = "delay"
-  CATEGORY = "FlowScale/Utils/Time"
-  
-  def delay(self, delay: int):
-    time.sleep(delay)
-    return "Delayed by {} seconds".format(delay)
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "delay": ("INT", {"default": 1, "min": 0, "max": 60, "step": 1})
+            },
+            "optional": {
+                "trigger": ("*",)
+            }
+        }
+        
+    @classmethod
+    def VALIDATE_INPUTS(s, input_types):
+        return True
+    
+    RETURN_TYPES = ("*",)
+    RETURN_NAMES = ("delayed_response",)
+    FUNCTION = "delay"
+    CATEGORY = "FlowScale/Utils/Time"
+    
+    def delay(self, delay: int, trigger=None):
+        logger.info(f"Delaying for {delay} seconds...")
+        time.sleep(delay)
+        # Return the trigger input as-is after the delay
+        return (trigger,)
